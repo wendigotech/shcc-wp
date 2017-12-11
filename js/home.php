cc2c7212ac762ee7586859e4e54aad201b1b94bd
wendigotech <wendigotech@gmail.com>
2017-12-11 15:41:17 +0100

    restructure, header.php, footer.php, jumbotron customizer with overlay

---
+++ js/home.php
@@ -0,0 +1,120 @@
+<?php
+get_header(); ?>
+
+      <section class="quote"> 
+          <div class="container"> 
+              <div class="row centered"> 
+                  <div class="col-md-1"></div>                         
+                  <div class="col-md-10"> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-quote.png" class="centered"> 
+                      <p><?php _e( '$38,332,931 jury trial verdict for fraud, breach of fiduciary duty and professional negligence on behalf of the creator of Tae Bo, Billy Blanks, his wife Gayle Blanks, and their company, BG Star Productions, Inc. against their former law firm. Billy Blanks, et al. v. Seyfarth Shaw LLP, et al., Los Angeles Superior Court case number BC308355.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-1"></div>                         
+              </div>                     
+          </div>                 
+      </section>
+      <section class="two-col" id="about"> 
+          <div class="container"> 
+              <div class="row"> 
+                  <div class="col-md-6 first"> 
+                      <h3><?php _e( 'Together We Can Put a Stop To It', 'shcc_wp' ); ?></h3> 
+                      <p><?php _e( 'Before that can happen it is very important to understand what sexual harassment is.', 'shcc_wp' ); ?></p> 
+                      <p><?php _e( 'The basis for sexual harassment legal protection is Title VII of the Civil Rights Act of 1974. It is unlawful to harass a person (an applicant or employee) because of that person’s sex. Harassment can include “sexual harassment” or unwelcome sexual advances. It is also unlawful to request sexual favors. Whether it is verbal or physical, harassment of a sexual nature is unlawful.', 'shcc_wp' ); ?></p> 
+                      <p><?php _e( 'Every state in the union has enacted some form of sexual harassment legislation. The model version is the one used by the federal government. Some states provide for workman’s compensation. Some allow damages for personal injury and some allow punitive damages as well.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-6 second"> 
+                      <h3><?php _e( 'Sexual harassment may include:', 'shcc_wp' ); ?></h3> 
+                      <!--<ul>
+	<li>The victim as well as the harasser may be a woman or a man. The victim does not have to be of the opposite sex.</li>
+<li>The harasser can be the victim’s supervisor, an agent of the employer, a supervisor in another area, a co-worker, or a non-employee.</li>
+<li>The victim does not have to be the person harassed but could be anyone affected by the offensive conduct.</li>
+<li>Unlawful sexual harassment may occur without economic injury to or discharge of the victim.</li>
+<li>The harasser’s conduct must be unwelcome.</li>
+</ul>-->                             
+                      <div class="row"> 
+                          <div class="col-md-1"> 
+                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-one.png"> 
+                          </div>                                 
+                          <div class="col-md-11"> 
+                              <p><?php _e( 'The victim as well as the harasser may be a woman or a man. The victim does not have to be of the opposite sex.', 'shcc_wp' ); ?></p> 
+                          </div>                                 
+                          <div class="col-md-1"> 
+                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-two.png"> 
+                          </div>                                 
+                          <div class="col-md-11"> 
+                              <p><?php _e( 'The harasser can be the victim’s supervisor, an agent of the employer, a supervisor in another area, a co-worker, or a non-employee.', 'shcc_wp' ); ?></p> 
+                          </div>                                 
+                          <div class="col-md-1"> 
+                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-three.png"> 
+                          </div>                                 
+                          <div class="col-md-11"> 
+                              <p><?php _e( 'The victim does not have to be the person harassed but could be anyone affected by the offensive conduct.', 'shcc_wp' ); ?></p> 
+                          </div>                                 
+                          <div class="col-md-1"> 
+                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-four.png"> 
+                          </div>                                 
+                          <div class="col-md-11"> 
+                              <p><?php _e( 'Unlawful sexual harassment may occur without economic injury to or discharge of the victim.', 'shcc_wp' ); ?></p> 
+                          </div>                                 
+                          <div class="col-md-1"> 
+                              <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-five.png"> 
+                          </div>                                 
+                          <div class="col-md-11"> 
+                              <p><?php _e( 'The harasser’s conduct must be unwelcome.', 'shcc_wp' ); ?></p> 
+                          </div>                                 
+                      </div>                             
+                  </div>                         
+              </div>                     
+      </section>
+      <section class="help centered"> 
+          <div class="container"> 
+              <div class="row"> 
+                  <div class="col-md-3"></div>                         
+                  <div class="col-md-6"> 
+                      <h3><?php _e( 'How We Can Help', 'shcc_wp' ); ?></h3> 
+                      <p><?php _e( 'If you or your loved one are in need of legal assistance concerning a potential lawsuit you should get some legal advice sooner rather than later.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-3"></div>                         
+              </div>                     
+              <div class="row four-col"> 
+                  <div class="col-md-3"> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/HSCC-icon1.png"> 
+                      <p class="title"><strong><?php _e( 'Case Evaluation', 'shcc_wp' ); ?></strong></p> 
+                      <p><?php _e( 'A FREE, No Obligations, No Hassle, Case Evaluation.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-3"> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/HSCC-icon2.png"> 
+                      <p class="title"><strong><?php _e( 'Consumer Care', 'shcc_wp' ); ?></strong></p> 
+                      <p><?php _e( 'Personal Service, Speak Directly With A Responsible Lawyer. No Call Centers.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-3"> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/HSCC-icon3.png"> 
+                      <p class="title"><strong><?php _e( 'Experience', 'shcc_wp' ); ?></strong></p> 
+                      <p><?php _e( 'Over 100 Years Of Experience With Over $200 Million In Recovery For Victims.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+                  <div class="col-md-3"> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/HSCC-icon4.png"> 
+                      <p class="title"><strong><?php _e( 'Safety', 'shcc_wp' ); ?></strong></p> 
+                      <p><?php _e( 'We promise to keep you secure and anonymous.', 'shcc_wp' ); ?></p> 
+                  </div>                         
+              </div>                     
+          </div>                 
+      </section>
+      <section class="mind-section"> 
+          <div class="container"> 
+              <div class="row"> 
+                  <div class="col-md-2"></div>                         
+                  <div class="col-md-8 centered"> 
+                      <h3><?php _e( 'Peace Of Mind Could Be 30 Seconds Away', 'shcc_wp' ); ?></h3>
+                      <br> 
+                      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/SHCC-mind.png">
+                      <br>
+                      <br> 
+                      <a class="btn btn-primary btn-xl" href="#services"><?php _e( 'Fill Out A Free Case Evaluation Form Now', 'shcc_wp' ); ?></a> 
+                  </div>                         
+                  <div class="col-md-2"></div>                         
+              </div>                     
+          </div>                 
+      </section>            
+
+<?php get_footer(); ?>
